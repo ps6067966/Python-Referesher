27f94bd01a3f4e44b55f9b13c39a43ded24dd4a3
Pratap Singh <ps6067966@gmail.com>
2021-11-25 14:36:38 +0530

    function with parameter and return statement

--- pythonrefresher.py
+++ pythonrefresher.py
@@ -14,8 +14,17 @@ This multi line comment.
 
 # this is single line comment. 
 
-if age != 14:
-    print("You are older than 18")
-    print("This is another line")
-else:
-    print("you are younger than 18")
+# if age != 14:
+#     print("You are older than 18")
+#     print("This is another line")
+# else:
+#     print("you are younger than 18")
+
+
+
+def hello(name, age=0):
+    return "Hello {} you are {} years old".format(name, age)
+
+
+sentence = hello("Mark", 15)
+print(sentence)
