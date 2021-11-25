c3a85f954aebdf67183e5f8f76c28b40da9fe16e
Pratap Singh <ps6067966@gmail.com>
2021-11-25 14:26:34 +0530

    if else and comments in python

--- pythonrefresher.py
+++ pythonrefresher.py
@@ -1,4 +1,21 @@
 age = 22
 name = 'Pratap'
 
-print(f"Hello my name is {name} and I am {age} years old")
+isTodayCold = True
+
+# Print name and age and string interpolation
+# print("Hello my name is {} and I am {} years old".format(name,age))
+
+""" 
+
+This multi line comment. 
+
+"""
+
+# this is single line comment. 
+
+if age != 14:
+    print("You are older than 18")
+    print("This is another line")
+else:
+    print("you are younger than 18")
